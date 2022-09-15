56b20f12174c23ca5dcbc0f12744ef3ff96094eb
Sárándi <sarandi.balint@gmail.com>
2022-09-15 09:19:09 +0200

    10 véletlen pontos tömb kiíratása

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -1,15 +1,27 @@
 package hu.petrik.pontoop;
 
+import static java.lang.System.*;
+
 import hu.petrik.pontoop.pont.Pont;
 
 public class Main {
     public static void main(String[] args) {
         Pont p1 = new Pont();
-        Pont p2 = new Pont(0,0);
-        Pont p3 = new Pont(3,4);
+        Pont p2 = new Pont(0, 0);
+        Pont p3 = new Pont(3, 4);
+        Pont p4 = new Pont(100);
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+//        System.out.println(p1);
+//        System.out.println(p2);
+//        System.out.println(p3);
+//        System.out.println(p4);
+
+        Pont[] pontok = new Pont[10];
+        for (int i = 0; i < pontok.length; i++) {
+            pontok[i] = new Pont(100);
+        }
+        for (Pont p : pontok) {
+            out.println(p);
+        }
     }
 }
