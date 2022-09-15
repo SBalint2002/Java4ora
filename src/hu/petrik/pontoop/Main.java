d93de93c227d165bdd911ef998ba413f0b1313d6
Sárándi <sarandi.balint@gmail.com>
2022-09-15 09:11:42 +0200

    Pont osztály konstruktor

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -1,7 +1,15 @@
 package hu.petrik.pontoop;
 
+import hu.petrik.pontoop.pont.Pont;
+
 public class Main {
     public static void main(String[] args) {
+        Pont p1 = new Pont();
+        Pont p2 = new Pont(0,0);
+        Pont p3 = new Pont(3,4);
 
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
     }
 }
--- src/hu/petrik/pontoop/pont/Pont.java
+++ src/hu/petrik/pontoop/pont/Pont.java
@@ -1,17 +1,31 @@
-package pont;
+package hu.petrik.pontoop.pont;
 
 public class Pont {
     private int x;
     private int y;
 
-    public Pont(){
-        x = 0;
-        y = 0;
+    public Pont() {
+        this.x = 0;
+        this.y = 0;
     }
 
-    public Pont(int x, int y){
-        x = x;
-        y = y;
+    public Pont(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    
+
+    public Pont(int n) {
+        this.x = koordinataGeneralasa(n);
+        this.y = koordinataGeneralasa(n);
+    }
+
+    private static int koordinataGeneralasa(int n) {
+        return (int) (Math.random() * (2 * n) + 1) - n;
+    }
+
+    @Override
+    public String toString() {
+        //return "(" + x + ", " + y + ')';
+        return String.format("(%d, %d)", this.x, this.y);
+    }
 }
