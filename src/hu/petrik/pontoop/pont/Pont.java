620612cf769bdabd23c087bc961b4d3759fac34f
Sárándi <sarandi.balint@gmail.com>
2022-09-15 08:44:02 +0200

    Mappa áthelyezve

---
+++ src/hu/petrik/pontoop/pont/Pont.java
@@ -0,0 +1,17 @@
+package pont;
+
+public class Pont {
+    private int x;
+    private int y;
+
+    public Pont(){
+        x = 0;
+        y = 0;
+    }
+
+    public Pont(int x, int y){
+        x = x;
+        y = y;
+    }
+    
+}
